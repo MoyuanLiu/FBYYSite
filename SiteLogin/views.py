387bda77ce30ae0c07f17555bb364720af12e984
MoyuanLiu <jljuliumoyuan@163.com>
2019-01-23 15:41:20 +0800

    fix login view

--- SiteLogin/views.py
+++ SiteLogin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    t = loader.get_template('login.html')
+    return t.render()
