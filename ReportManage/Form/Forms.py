e46198dc1f8d8e352d0883ea22442340b786572a
Marvin Liu <jljuliumoyuan@163.com>
2019-04-03 16:21:48 +0800

    update form

--- ReportManage/Form/Forms.py
+++ ReportManage/Form/Forms.py
@@ -11,3 +11,29 @@ class CtsQueryForm(forms.Form):
 class CtsMakeForm(forms.Form):
     ctsmakedate = forms.DateField(required=False,widget=forms.DateInput(attrs={'class': 'form-control','placeholder':'制作日期:yyyy-mm-dd'}))
     ctsmakedepart = forms.CharField(required=False)
+
+class KckQueryForm(forms.Form):
+    kckproductid = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '宝贝ID', 'class': 'form-control'}))
+    kckproductcolornum = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '货号(到色)', 'class': 'form-control'}))
+    kckproductcategory = forms.CharField(required=False, max_length=20, widget=forms.TextInput(attrs={'placeholder': '类目', 'class': 'form-control'}))
+    kckshelfdateflag = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    kckshelfdate = forms.DateField(required=False, widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': '上架日期:yyyy-mm-dd'}))
+    kckseldepart = forms.CharField(required=False)
+    kckselstore = forms.CharField(required=False)
+
+class KckAddForm(forms.Form):
+    kckproductid = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '宝贝ID', 'class': 'form-control'}))
+    kckproductcolornum = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '货号(到色)', 'class': 'form-control'}))
+    kckproductcategory = forms.CharField(required=False, max_length=20, widget=forms.TextInput(attrs={'placeholder': '类目', 'class': 'form-control'}))
+    kckshelfdate = forms.DateField(required=False, widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': '上架日期:yyyy-mm-dd'}))
+    kckseldepart = forms.CharField(required=False)
+    kckselstore = forms.CharField(required=False)
+
+class KckEditForm(forms.Form):
+    kckid = forms.CharField(required=False)
+    kckproductid = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '宝贝ID', 'class': 'form-control'}))
+    kckproductcolornum = forms.CharField(required=False, max_length=20, widget=forms.TextInput(attrs={'placeholder': '货号(到色)', 'class': 'form-control'}))
+    kckproductcategory = forms.CharField(required=False, max_length=20,widget=forms.TextInput(attrs={'placeholder': '类目', 'class': 'form-control'}))
+    kckshelfdate = forms.DateField(required=False, widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': '上架日期:yyyy-mm-dd'}))
+    kckseldepart = forms.CharField(required=False)
+    kckselstore = forms.CharField(required=False)
